e64deba713fdf75b142db82bee62d696f58f9e4c
Ruben Juarez <exacrefu@gmail.com>
2023-03-12 18:29:40 -0600

    Una version de permutaciones

--- Permutaciones.py
+++ Permutaciones.py
@@ -30,7 +30,7 @@ def recursive(lista, cadena):
 
 def imprime():
     #Solo imprime hola
-    print("hello este es otro cambio, hice un cambio en git")
+    print("hello este es otro cambio, hice un cambio en git/di de alta llavas ssh")
 
     
 cadena = input("Ingresa cadena: ")
